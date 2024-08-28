491496e9408f4919bf36d15406abbf5fac600cad
eucamil4 <163136842+eucamil4@users.noreply.github.com>
2024-08-28 07:58:16 -0300

    js

---
+++ script.js
@@ -0,0 +1,27 @@
+var nome = "John";
+let idade = 25;
+const pi = 3.14;
+
+let a = 5;
+let b = 10;
+let soma = a + b;
+console.log (soma);
+
+let a = 5;
+let b = 10;
+
+function multiplica (x, y) {
+    return x * y;
+}
+let produto = multiplica(a, b);
+console.log(produto);
+
+let frutas = ['Apple', 'Banana', 'Cherry'];
+console.log(frutas[11]);
+
+let pessoa = {
+    primeiroNome: 'John',
+    sobrenome: 'Delon',
+    age: 25
+};
+console.log(pessoa.primeiroNome);
